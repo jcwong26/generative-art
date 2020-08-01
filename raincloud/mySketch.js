b36b00af783082f3eee6fe98ca6b88a568d07954
Jillian <jcwong326@gmail.com>
2020-08-01 00:53:59 -0700

    change background with key press

--- raincloud/mySketch.js
+++ raincloud/mySketch.js
@@ -1,29 +1,44 @@
 let raindrops = [];
 let r_size = 8;
+let light = true;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	c1 = color(10, 112, 245);
-	c2 = color(135, 208, 250);
+	dblue = color(10, 112, 245);
+	lblue = color(135, 208, 250);
+	orange = color(232, 81, 26);
 }
 
 function draw() {
 	//background(196, 227, 255);
-	//Gradient background
-	for (let i = 0; i <= windowHeight; i++) {
-      let inter = map(i, 0, windowHeight, 0, 1);
-      let c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(0, i, windowWidth, i);
-  }
-	
+	sky(light);
+	rainfall();	
 	cloud();
-	rain();	
 	if(mouseIsPressed) {
-		add_rain(mouseX,mouseY);
+		rain(mouseX,mouseY);
 	}
 }
 
+function keyPressed() {
+	light = !light;
+}
+
+function sky(light) {
+	if(light) {
+		c2 = lblue;
+	}
+	else {
+		c2 = orange;
+	}
+	
+	//Gradient background
+	for (let i = 0; i <= windowHeight; i++) {
+      let inter = map(i, 0, windowHeight, 0, 1);
+      let c = lerpColor(dblue, c2, inter);
+      stroke(c);
+      line(0, i, windowWidth, i);
+  }
+}
 
 class Raindrop {
 	constructor(x,y,size,velocity) {
@@ -43,20 +58,21 @@ class Raindrop {
 		for (let j = 2; j < this.size; j++ ) {
       ellipse(this.x, this.y + j*4, j*2, j*2);
   	}
-	}
-	
+	}	
 }
 
 
 function cloud() {
 	noStroke();
 	fill(255);
-	ellipse(mouseX,mouseY,150);
-	ellipse(mouseX+60,mouseY+15,100);
-	ellipse(mouseX-60,mouseY+15,100);
+	ellipse(mouseX,mouseY,140);
+	ellipse(mouseX+65,mouseY+15,100);
+	ellipse(mouseX-65,mouseY+20,90);
+	ellipse(mouseX+110,mouseY+30,50);
+	ellipse(mouseX-105,mouseY+30,60);
 }
 
-function rain() {
+function rainfall() {
   for(let i = 0; i<raindrops.length; i++) {
   	raindrops[i].update();
 		raindrops[i].draw();
@@ -66,10 +82,10 @@ function rain() {
   }
 }
 
-function add_rain(x,y) {
+function rain(x,y) {
 	let r_num = 3;
 	for (let i = 1; i < r_num; i++) {
-		x_pos = x + random(-100,100);
+		x_pos = x + random(-120,120);
 		y_pos = y + 30;
 		size = random(4,6);
 		velocity = random(5,10);
